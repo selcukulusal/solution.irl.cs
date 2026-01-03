f6d1f9e9df799afcde34c69d21c73c87ba519cfd
Selçuk Ulusal <115923621+selcukulusal@users.noreply.github.com>
2026-01-03 14:23:23 -0800

    Add files via upload

---
+++ PowerNotif.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Solution.IRL;
+
+class PowerNotif
+{public static void Main(string[]args)
+
+    {string electric = "on";
+        if(electric == "on") {Console.WriteLine("Lights are on!");}
+        else{Console.WriteLine("UPS is Active");}}}
+
+
+
+
+        
